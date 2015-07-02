f513f2ecbf3661b8839a3bbf80335af23b384604
mohanfyi <mohanfyi@users.noreply.github.com>
2015-07-02 14:32:12 +0530

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,13 @@
+var winston = require('winston');
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ json: false, timestamp: true }),
+    new winston.transports.File({ filename: __dirname + '/logs/app.log', json: false })
+  ],
+  exceptionHandlers: [
+    new (winston.transports.Console)({ json: false, timestamp: true }),
+    new winston.transports.File({ filename: __dirname + '/logs/error.log', json: false })
+  ],
+  exitOnError: false
+});
+module.exports = logger;
